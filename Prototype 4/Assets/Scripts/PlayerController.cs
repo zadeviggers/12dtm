9deeef839f2a999bd6dfab48cd08152c150fe7cd
Zade Viggers <zade.viggers@gmail.com>
2022-03-21 11:08:52 +1300

    Add powerup

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private GameObject cameraFocusPoint;
     private Rigidbody playerRB;
+    private bool hasPowerup = false;
     public float speed;
+    public float powerupExplosionStrength;
+    public float powerupExplosionRadius;
+    public float powerupExplosionUpwardsModifier;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +24,26 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRB.AddForce(cameraFocusPoint.transform.forward * speed * forwardInput);
+    }
+
+    // Powerups
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    // Enemy collisions
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log($"Powerup triggered by {collision.gameObject.name}");
+            Rigidbody enemyRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+            enemyRigidBody.AddExplosionForce(powerupExplosionStrength, transform.position, powerupExplosionRadius, powerupExplosionUpwardsModifier, ForceMode.Impulse);
+        }
     }
 }
